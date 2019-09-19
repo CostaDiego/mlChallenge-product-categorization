e931ff8ec3dd47fc91f0710a67ad7e059d019936
Diego Costa <diego.szct@gmail.com>
2019-09-19 15:22:10 -0300

    Improve the memory usage

--- algorithm/dataProcessing.py
+++ algorithm/dataProcessing.py
@@ -4,9 +4,9 @@ from nltk import sent_tokenize, word_tokenize, pos_tag
 
 STANDARD_BATCH_SIZE = 10000
 
-def tokenization(data, target, batchSize = STANDARD_BATCH_SIZE, nbRows = None):
+def tokenization(data, target,savePath = None, batchSize = STANDARD_BATCH_SIZE, nbRows = None):
     print("Initializing Tokenization.")
-    finalData = None
+
     if isinstance(data, pd.DataFrame):
         print("Using Data Frame.")
         data[str(target)] = data[str(target)].map(sent_tokenize)
@@ -14,29 +14,50 @@ def tokenization(data, target, batchSize = STANDARD_BATCH_SIZE, nbRows = None):
             [
                 word_tokenize(title) for title in titles
             ])
-        finalData = data
+        return data
 
     elif isinstance(data, str):
-        processedData = []
-        print("Importing file from path: {path}".format(path = str(data)))
+        if isinstance(savePath, str):
 
-        for batch in pd.read_csv(str(data), chunksize=batchSize, nrows=nbRows):
-            data = batch
-            data[str(target)] = data[str(target)].map(sent_tokenize)
-            data[str(target)] = data[str(target)].map(lambda titles: 
-                [
-                    word_tokenize(title) for title in titles
-                ])
-            processedData.append(batch)
+            print("Importing file from path: {path}".format(path = str(data)))
+            print("Saving file on path: {path}".format(path = str(savePath)))
+            FtIndex = True
+            for batch in pd.read_csv(str(data), chunksize=batchSize, nrows=nbRows):
+                batch[str(target)] = batch[str(target)].map(sent_tokenize)
+                batch[str(target)] = batch[str(target)].map(lambda titles: 
+                    [
+                        word_tokenize(title) for title in titles
+                    ])
+                
+                batch.to_csv(str(savePath),
+                                index = False,
+                                header = FtIndex,
+                                mode = "a"
+                                    )
+                if FtIndex:
+                    FtIndex = False
 
-            del(data)
-            gc.collect()
-        finalData = pd.concat(processedData, ignore_index = True)
+                del(batch)
+                gc.collect()
+        else:
+            processedData = []
+            print("Importing file from path: {path}".format(path = str(data)))
+
+            for batch in pd.read_csv(str(data), chunksize=batchSize, nrows=nbRows):
+                batch[str(target)] = batch[str(target)].map(sent_tokenize)
+                batch[str(target)] = batch[str(target)].map(lambda titles: 
+                    [
+                        word_tokenize(title) for title in titles
+                    ])
+                processedData.append(batch)
+
+                del(batch)
+                gc.collect()
+            finalData = pd.concat(processedData, ignore_index = True)
+            return finalData
 
     else:
         raise Exception("Data Format not supported. ",
             "Please input a Pandas DataFrame or a path for a 'csv' file.")
     
     print("Tokenization finished.")
-
-    return finalData
